bb86cec8067d6ac9527d72c8e12c2b5be84bb53f
Jase Seeley <jcseeley31590@yahoo.com>
2022-03-18 13:18:51 -0700

    update Home Index and _Layout.

--- Factory/Controllers/HomeController.cs
+++ Factory/Controllers/HomeController.cs
@@ -1,13 +1,22 @@
+using Factory.Models;
 using Microsoft.AspNetCore.Mvc;
+using System.Linq;
 
 namespace Factory.Controllers
 {
   public class HomeController : Controller
   {
-    [HttpGet("/")]
+    private readonly FactoryContext _db;
+
+    public HomeController(FactoryContext db)
+    {
+      _db = db;
+    }
     public ActionResult Index()
     {
-      ViewBag.PageTitle = "Welcome to the To Do List!";
+      ViewBag.Engineers = _db.Engineers.ToList();
+      ViewBag.Machines = _db.Machines.ToList();
+      ViewBag.PageTitle = "Welcome to the Factory Manager!";
       return View();
     }
   }
