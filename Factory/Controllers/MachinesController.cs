59e43bdf0463db3fed9c99e0039004f9601aa15b
Jase Seeley <jcseeley31590@yahoo.com>
2022-03-20 13:25:47 -0700

    add AddEngineer page and DeleteEngineer route to Machines.

--- Factory/Controllers/MachinesController.cs
+++ Factory/Controllers/MachinesController.cs
@@ -45,5 +45,33 @@ namespace Factory.Controllers
       ViewBag.PageTitle = "Machine Details";
       return View(machine);
     }
+
+    public ActionResult AddEngineer(int id)
+    {
+      Machine machine = _db.Machines.FirstOrDefault(machine => machine.MachineId == id);
+      ViewBag.EngineerId = new SelectList(_db.Engineers, "EngineerId", "EngineerName");
+      ViewBag.PageTitle = "Add Engineer";
+      return View(machine);
+    }
+
+    [HttpPost]
+    public ActionResult AddEngineer(Machine machine, int engineerId)
+    {
+      if (engineerId != 0)
+      {
+        _db.RepairLicenses.Add(new RepairLicense() { EngineerId = engineerId, MachineId = machine.MachineId });
+        _db.SaveChanges();
+      }
+      return RedirectToAction("Details", new { id = machine.MachineId });
+    }
+
+    [HttpPost]
+    public ActionResult DeleteEngineer(int joinId)
+    {
+      RepairLicense joinEntry = _db.RepairLicenses.FirstOrDefault(entry => entry.RepairLicenseId == joinId);
+      _db.RepairLicenses.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Details", new { id = joinEntry.MachineId });
+    }
   }
 }
