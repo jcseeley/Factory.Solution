b9d96524075b56f4386879015e348448ba66ebfe
Jase Seeley <jcseeley31590@yahoo.com>
2022-03-20 12:09:55 -0700

    add Machines Index page.

---
+++ Factory/Controllers/MachinesController.cs
@@ -0,0 +1,25 @@
+using Factory.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Factory.Controllers
+{
+  public class MachinesController : Controller
+  {
+    private readonly FactoryContext _db;
+
+    public MachinesController(FactoryContext db)
+    {
+      _db = db;
+    }
+
+    public ActionResult Index()
+    {
+      ViewBag.PageTitle = "Machines";
+      return View(_db.Machines.ToList());
+    }
+  }
+}
